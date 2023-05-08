ca14e1937ad9ed3b12f0f5938facf84584337b93
Daniel Vasquez <nielvas.dev@gmail.com>
2023-05-08 16:18:48 -0700

    refactor: separate utility functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,18 @@
-#include <iostream>
-#include <vector>
+#include "webgpu-release.h"
+#include "webgpu-utils.h"
+
 #include <GLFW/glfw3.h>
 #include <webgpu/webgpu.h>
 #include <glfw3webgpu.h>
-#include "webgpu-release.h"
+
+#include <cassert>
+#include <iostream>
+#include <vector>
+
 #ifdef WEBGPU_BACKEND_WGPU
 #include <webgpu/wgpu.h>
 #define wgpuInstanceRelease wgpuInstanceDrop
 #endif
-
-/**
- * Utility function to get a WebGPU adapter, so that
- *     WGPUAdapter adapter = requestAdapter(options);
- * is roughly equivalent to
- *     const adapter = await navigator.gpu.requestAdapter(options);
- */
-WGPUAdapter requestAdapter(WGPUInstance instance, WGPURequestAdapterOptions const *options)
-{
-  // A simple structure holding the local information shared with the
-  // onAdapterRequestEnded callback.
-  struct UserData
-  {
-    WGPUAdapter adapter = nullptr;
-    bool requestEnded = false;
-  };
-  UserData userData;
-
-  // Callback called by wgpuInstanceRequestAdapter when the request returns
-  // This is a C++ lambda function, but could be any function defined in the
-  // global scope. It must be non-capturing (the brackets [] are empty) so
-  // that it behaves like a regular C function pointer, which is what
-  // wgpuInstanceRequestAdapter expects (WebGPU being a C API). The workaround
-  // is to convey what we want to capture through the pUserData pointer,
-  // provided as the last argument of wgpuInstanceRequestAdapter and received
-  // by the callback as its last argument.
-  auto onAdapterRequestEnded = [](WGPURequestAdapterStatus status, WGPUAdapter adapter, char const *message, void *pUserData)
-  {
-    UserData &userData = *reinterpret_cast<UserData *>(pUserData);
-    if (status == WGPURequestAdapterStatus_Success)
-    {
-      userData.adapter = adapter;
-    }
-    else
-    {
-      std::cout << "Could not get WebGPU adapter: " << message << std::endl;
-    }
-    userData.requestEnded = true;
-  };
-
-  // Call to the WebGPU request adapter procedure
-  wgpuInstanceRequestAdapter(
-      instance /* equivalent of navigator.gpu */,
-      options,
-      onAdapterRequestEnded,
-      (void *)&userData);
-
-  // In theory we should wait until onAdapterReady has been called, which
-  // could take some time (what the 'await' keyword does in the JavaScript
-  // code). In practice, we know that when the wgpuInstanceRequestAdapter()
-  // function returns its callback has been called.
-  assert(userData.requestEnded);
-
-  return userData.adapter;
-}
 
 int main(int, char **)
 {
