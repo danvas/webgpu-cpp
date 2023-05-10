22c6866aae1713631deee1fa81eb0d53705ff00b
Daniel Vasquez <nielvas.dev@gmail.com>
2023-05-09 22:03:35 -0700

    feat: first triangle

--- src/main.cpp
+++ src/main.cpp
@@ -83,8 +83,12 @@ int main(int, char **)
   Queue queue = device.getQueue();
 
   std::cout << "Creating swapchain device..." << std::endl;
+#ifdef WEBGPU_BACKEND_WGPU
   TextureFormat swapChainFormat = surface.getPreferredFormat(adapter);
-  SwapChainDescriptor swapChainDesc = {};
+#else
+  TextureFormat swapChainFormat = TextureFormat::BGRA8Unorm;
+#endif
+  SwapChainDescriptor swapChainDesc;
   swapChainDesc.width = 640;
   swapChainDesc.height = 480;
   swapChainDesc.usage = TextureUsage::RenderAttachment;
@@ -92,6 +96,124 @@ int main(int, char **)
   swapChainDesc.presentMode = PresentMode::Fifo;
   SwapChain swapChain = device.createSwapChain(surface, swapChainDesc);
   std::cout << "Swapchain: " << swapChain << std::endl;
+
+  std::cout << "Creating shader module..." << std::endl;
+  const char *shaderSource = R"(
+@vertex
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
+	var p = vec2f(0.0, 0.0);
+	if (in_vertex_index == 0u) {
+		p = vec2f(-0.5, -0.5);
+	} else if (in_vertex_index == 1u) {
+		p = vec2f(0.5, -0.5);
+	} else {
+		p = vec2f(0.0, 0.5);
+	}
+	return vec4f(p, 0.0, 1.0);
+}
+
+@fragment
+fn fs_main() -> @location(0) vec4f {
+    return vec4f(0.0, 0.4, 1.0, 1.0);
+}
+)";
+  ShaderModuleDescriptor shaderDesc;
+#ifdef WEBGPU_BACKEND_WGPU
+  shaderDesc.hintCount = 0;
+  shaderDesc.hints = nullptr;
+#endif
+
+  // Use the extension mechanism to load a WGSL shader source code
+  ShaderModuleWGSLDescriptor shaderCodeDesc;
+  // Set the chained struct's header
+  shaderCodeDesc.chain.next = nullptr;
+  shaderCodeDesc.chain.sType = SType::ShaderModuleWGSLDescriptor;
+  // Connect the chain
+  shaderDesc.nextInChain = &shaderCodeDesc.chain;
+
+#ifdef WEBGPU_BACKEND_WGPU
+  shaderCodeDesc.code = shaderSource;
+#else
+  shaderCodeDesc.source = shaderSource;
+#endif
+
+  ShaderModule shaderModule = device.createShaderModule(shaderDesc);
+  std::cout << "Shader module: " << shaderModule << std::endl;
+
+  std::cout << "Creating render pipeline..." << std::endl;
+  RenderPipelineDescriptor pipelineDesc;
+
+  // Vertex fetch
+  // (We don't use any input buffer so far)
+  pipelineDesc.vertex.bufferCount = 0;
+  pipelineDesc.vertex.buffers = nullptr;
+
+  // Vertex shader
+  pipelineDesc.vertex.module = shaderModule;
+  pipelineDesc.vertex.entryPoint = "vs_main";
+  pipelineDesc.vertex.constantCount = 0;
+  pipelineDesc.vertex.constants = nullptr;
+
+  // Primitive assembly and rasterization
+  // Each sequence of 3 vertices is considered as a triangle
+  pipelineDesc.primitive.topology = PrimitiveTopology::TriangleList;
+  // We'll see later how to specify the order in which vertices should be
+  // connected. When not specified, vertices are considered sequentially.
+  pipelineDesc.primitive.stripIndexFormat = IndexFormat::Undefined;
+  // The face orientation is defined by assuming that when looking
+  // from the front of the face, its corner vertices are enumerated
+  // in the counter-clockwise (CCW) order.
+  pipelineDesc.primitive.frontFace = FrontFace::CCW;
+  // But the face orientation does not matter much because we do not
+  // cull (i.e. "hide") the faces pointing away from us (which is often
+  // used for optimization).
+  pipelineDesc.primitive.cullMode = CullMode::None;
+
+  // Fragment shader
+  FragmentState fragmentState;
+  pipelineDesc.fragment = &fragmentState;
+  fragmentState.module = shaderModule;
+  fragmentState.entryPoint = "fs_main";
+  fragmentState.constantCount = 0;
+  fragmentState.constants = nullptr;
+
+  // Configure blend state
+  BlendState blendState;
+  // Usual alpha blending for the color:
+  blendState.color.srcFactor = BlendFactor::SrcAlpha;
+  blendState.color.dstFactor = BlendFactor::OneMinusSrcAlpha;
+  blendState.color.operation = BlendOperation::Add;
+  // We leave the target alpha untouched:
+  blendState.alpha.srcFactor = BlendFactor::Zero;
+  blendState.alpha.dstFactor = BlendFactor::One;
+  blendState.alpha.operation = BlendOperation::Add;
+
+  ColorTargetState colorTarget;
+  colorTarget.format = swapChainFormat;
+  colorTarget.blend = &blendState;
+  colorTarget.writeMask = ColorWriteMask::All; // We could write to only some of the color channels.
+
+  // We have only one target because our render pass has only one output color
+  // attachment.
+  fragmentState.targetCount = 1;
+  fragmentState.targets = &colorTarget;
+
+  // Depth and stencil tests are not used here
+  pipelineDesc.depthStencil = nullptr;
+
+  // Multi-sampling
+  // Samples per pixel
+  pipelineDesc.multisample.count = 1;
+  // Default value for the mask, meaning "all bits on"
+  pipelineDesc.multisample.mask = ~0u;
+  // Default value as well (irrelevant for count = 1 anyways)
+  pipelineDesc.multisample.alphaToCoverageEnabled = false;
+
+  // Pipeline layout
+  pipelineDesc.layout = nullptr;
+
+  RenderPipeline pipeline = device.createRenderPipeline(pipelineDesc);
+  std::cout << "Render pipeline: " << pipeline << std::endl;
 
   while (!glfwWindowShouldClose(window))
   {
@@ -123,6 +245,13 @@ int main(int, char **)
     renderPassDesc.timestampWriteCount = 0;
     renderPassDesc.timestampWrites = nullptr;
     RenderPassEncoder renderPass = encoder.beginRenderPass(renderPassDesc);
+
+    // In its overall outline, drawing a triangle is as simple as this:
+    // Select which render pipeline to use
+    renderPass.setPipeline(pipeline);
+    // Draw 1 instance of a 3-vertices shape
+    renderPass.draw(3, 1, 0, 0);
+
     renderPass.end();
 
     // This procedure is not handled by the wrapper because not standard,
